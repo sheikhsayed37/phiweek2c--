83b7469734b423019194a543a405a165163bc696
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-12-20 23:51:52 +0600

    11

---
+++ string3.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="sheikh mohammed sayed ali";
+    cout<< s.size()<< endl;
+}
---
+++ string5.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="sheikh mohammed sayed ali";
+    s.clear();  
+    if(s.empty()==true)
+    {
+       cout<<"EMPTY"<< endl;
+    }
+    else
+    {
+        cout <<"not empty"<<endl;
+    }
+}
