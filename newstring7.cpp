177f984d9c65167a7cb962df37f5c062240d833a
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-12-21 00:03:59 +0600

    3

---
+++ newstring7.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="sheikh mohammed sayed ali";
+   
+    if(s.empty()==true)
+    {
+       cout<<"EMPTY"<< endl;
+    }
+    else
+    {
+        cout <<"not empty"<<endl;
+    }
+}
---
+++ resizestring.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ string s="sheikh mohammed sayed ali";
+ s.resize(45,'2');
+ cout << s<< endl;
+
+}
