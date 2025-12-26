73593d8171b3b64169a44ec9cf6d5eb1c5351f7c
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-12-26 22:59:50 +0600

    34

---
+++ string_constructor.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+//string s="sheikh mohammed sayed ali";
+
+
+string s("sheikh mohammed sayed ali");// direct aibabe declare kora jay
+cout << s<< endl;
+
+string str("sheikh mohammed sayed ali",6);//6 means first 6 character
+cout << str<< endl;
+ 
+
+
+
+}
---
+++ string_constructor2.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="sheikh mohammed sayed ali";
+
+    string str(s,7);        //7 means starting from index 7 to end
+    cout << str << endl;
+    return 0;
+}
