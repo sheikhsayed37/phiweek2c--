49d2decd70384ac6253297308cb45658992b98c3
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-12-30 22:01:01 +0600

    13

---
+++ string_sort.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="sheikh mohammed sayed ali"; //ata alphabetical letter onojaiee serially
+                                        // print korse j
+    sort(s.begin(),s.end());
+    
+    cout <<s<<" ";
+
+}
---
+++ stringcomstructor5.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //string s="sheikh mohammed sayed ali";
+
+  string s(6,'s');  // 6 means total 6 ta 's' print hobe 
+
+  cout << s<<endl;
+  
+
+}
