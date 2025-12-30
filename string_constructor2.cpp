67fe67f5be25935749afe4b905edb3ebe1a5fe23
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-12-30 21:34:57 +0600

    14

--- string_constructor2.cpp
+++ string_constructor2.cpp
@@ -5,6 +5,7 @@ int main()
     string s="sheikh mohammed sayed ali";
 
     string str(s,7);        //7 means starting from index 7 to end
+    
     cout << str << endl;
     return 0;
 }
---
+++ stringcontructor3.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string  s="sheikh mohammed sayed ali";
+    string sa(s,3);    //3 means starting from index 3 teke sesh porjonto
+    cout << sa<<" ";
+}
