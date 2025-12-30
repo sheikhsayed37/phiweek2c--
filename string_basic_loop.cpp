abb6d77cd37eff63cc93ca58ebf79ad5f0ac4bac
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-12-30 23:44:06 +0600

    344

---
+++ string_basic_loop.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="sheikh mohammed saif ali";
+
+    for(int i=0;i<s.size();i++)
+
+    {
+    cout<< s[i]<<endl;
+
+    }
+    
+}
---
+++ string_newtypeloop2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="sheikh mohammed saif ali";
+    for(char c:s)
+    {
+        cout << c  << endl;  //akane c bosaba j bocco 
+                             //ata word by word print korbe 
+                             //setar shortcut way 
+    }
+}
