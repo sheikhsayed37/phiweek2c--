12141b0a0c1c3b848a1dc0d5c4101d20373abb40
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-12-31 23:59:26 +0600

    34

---
+++ contructor_onceagain.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class student
+{
+    public:
+    string name;
+    int roll;
+    int math;
+    int english;
+      student(string name,int roll,int math,int english)
+
+      {
+         this->name=name;
+         this->roll=roll;
+         this->math=math;
+         this->english=english;
+      }
+      void new1()
+      {
+        cout<< "hellow" <<" "<<name<< " "<< math+english<<" "<<endl;
+      }   //hellow er por nicer call  
+      
+      
+      
+};
+
+int main()
+{
+   student sayed("sayed ali",45,98,80);
+   sayed.new1();
+   student saif("saif ali",61,98,81);
+   saif.new1();
+    
+}
