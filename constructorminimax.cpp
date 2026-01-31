3a21c9dad1c98c19d7b17a728e24915f227b16b7
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-01-31 23:49:31 +0600

    4

--- constructorminimax.cpp
+++ constructorminimax.cpp
@@ -7,8 +7,11 @@ class student
     int roll;
     int marks;
 };
+
 int main()
+
 {
+
     int n;
     cin>>n;
     student a[n];
@@ -16,11 +19,11 @@ int main()
     {
         cin>> a[i].name>> a[i].roll>> a[i].marks;
     }
-
     int mn=INT_MAX;    // int max onk boro akta soinka jekane kicp akta raka ase
-                    
+
     for(int i=0;i<n;i++)
-    {
+   {
+      
        mn=min(a[i].marks,mn);  // 3<max thle mn e 3 gia bosbe erpr 4>3 abr 3 gia
                             // mn e bosbe 
 
